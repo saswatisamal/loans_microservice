096de0381a330c7416fdc0e7940e2e6f598fd81a
saswatisamal <saswatisamal02@gmail.com>
2022-08-27 00:23:46 +0530

    few modifications

--- src/main/java/com/in30minutes/loans/LoansApplication.java
+++ src/main/java/com/in30minutes/loans/LoansApplication.java
@@ -1,5 +1,7 @@
 package com.in30minutes.loans;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,8 +11,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(value={"com.in30minutes.loans.repository"})
 @EntityScan(value={"com.in30minutes.loans.model"})
 public class LoansApplication {
+	
+ static Logger logger= LoggerFactory.getLogger(LoansApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Loans application main method:::");
 		SpringApplication.run(LoansApplication.class, args);
 	}
 
